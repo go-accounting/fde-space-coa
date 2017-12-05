f3c5e2aaaffe41e0e86f7d6d03cd0383159e9e0a
Marcos César de Oliveira <mcesarhm@gmail.com>
2017-12-05 10:41:23 -0200

    Adapt to new config format

--- plugin/main.go
+++ plugin/main.go
@@ -6,41 +6,26 @@ import (
 	fdespacecoa "github.com/go-accounting/fde-space-coa"
 )
 
-var newSpace func(map[string]interface{}, *string, *string) (interface{}, error)
-var newKeyValueStore func(map[string]interface{}, *string) (interface{}, error)
-var LoadSymbolFunction func(string, string) (interface{}, error)
-
-func NewStoreAndAccountsRepository(
-	storeSettings map[string]interface{},
-	accountsRepositorySettings map[string]interface{},
-	user *string,
-	coaid *string,
-) (interface{}, interface{}, error) {
-	if newSpace == nil {
-		symbol, err := LoadSymbolFunction(storeSettings["PluginFile"].(string), "NewSpace")
-		if err != nil {
-			return nil, nil, err
-		}
-		newSpace = symbol.(func(map[string]interface{}, *string, *string) (interface{}, error))
-	}
-	if newKeyValueStore == nil {
-		symbol, err := LoadSymbolFunction(accountsRepositorySettings["PluginFile"].(string), "NewKeyValueStore")
-		if err != nil {
-			return nil, nil, err
-		}
-		newKeyValueStore = symbol.(func(map[string]interface{}, *string) (interface{}, error))
-	}
-	space, err := newSpace(storeSettings, user, coaid)
+func NewFdeStoreAndAccountsRepository(config map[string]interface{}, ss ...*string) (interface{}, error) {
+	space, err := cast(config["NewSpace"])(config, ss...)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
-	keyValueStore, err := newKeyValueStore(accountsRepositorySettings, user)
+	keyValueStore, err := cast(config["NewKeyValueStore"])(config, ss[0])
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
-	return fdespacecoa.NewStoreAndAccountsRepository(
+	s, ar, err := fdespacecoa.NewStoreAndAccountsRepository(
 		space.(deb.Space),
 		coa.NewCoaRepository(keyValueStore.(coa.KeyValueStore)),
-		coaid,
+		ss[1],
 	)
+	if err != nil {
+		return nil, err
+	}
+	return []interface{}{s, ar}, nil
+}
+
+func cast(v interface{}) func(map[string]interface{}, ...*string) (interface{}, error) {
+	return v.(func(map[string]interface{}, ...*string) (interface{}, error))
 }
